3b7e6a5467d0294a5f9a9e019ad889b9ad63cb75
Jack <38892053+JackHill-Dev@users.noreply.github.com>
2022-10-06 10:56:41 +0100

    Revert "Added missing semicolon"
    This reverts commit 3be5c5d1c57bca0c2c77aba1019da245f4066d36.

--- Asteroids/Game.cpp
+++ Asteroids/Game.cpp
@@ -220,7 +220,7 @@ void Game::UpdateGameData(float& dt,  char* buffer)
 		AsteroidDataPacket astData;
 		memcpy(&astData, &buffer[readIndex], sizeof(AsteroidDataPacket));
 		asteroids[astData.id]->isDestroyed = astData.Destroyed;
-		asteroids[astData.id]->velocity = astData.Velocity;
+		asteroids[astData.id]->velocity = astData.Velocity
 		////for (size_t i = 0; i < asteroids.size(); i++)
 		////{
 		//	
